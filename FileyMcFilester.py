52487ced7b5db664b26d323c8e89a9eee4c8c1b8
Olle Reberg <OlleReberg@users.noreply.github.com>
2026-09-18 12:58:59 +0200

    Added FileyMcFilester.py to branch

---
+++ FileyMcFilester.py
@@ -0,0 +1,10 @@
+#Add some code or smth
+myCoolVariable = []
+
+def OlleIsTheGreatest():
+    OlleIsCool = True
+    if OlleIsCool == True:
+        print("Ye, Olle is sooooo frickin cool!")
+
+while olleIsWinning == True:
+    OlleIsTheGreatest()
