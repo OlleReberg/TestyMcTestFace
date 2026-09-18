8a7ce2d300ff6f0555bc3d9601c7a8490d73f473
Olle Reberg <OlleReberg@users.noreply.github.com>
2026-09-18 13:05:02 +0200

    Create FileyMcFilester.py

---
+++ FileyMcFilester.py
@@ -0,0 +1,7 @@
+def OlleIsTheGreatest():
+    OlleIsCool = True
+    if OlleIsCool == True:
+        print("Ye, Olle is sooooo frickin amazing, everybody loves him and he never snores!!")
+olleIsWinning = True
+while olleIsWinning == True:
+    OlleIsTheGreatest()
